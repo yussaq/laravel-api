f7df53e7b1df7130b9840f4bfd4e234ce50d240d
Yussaqnf <yussaq.nf@gmail.com>
2026-06-01 09:33:30 +0700

    Auth

--- Modules/Auth/app/Http/Controllers/AuthController.php
+++ Modules/Auth/app/Http/Controllers/AuthController.php
@@ -1,54 +1,50 @@
 <?php
+
 namespace Modules\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
-{
-    $credentials = $request->validate([
-        'email' => ['required', 'email'],
-        'password' => ['required'],
-    ]);
-
-    if (!Auth::attempt($credentials)) {
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Email atau password salah'
+            ], 401);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('react')->plainTextToken;
         return response()->json([
-            'message' => 'Email atau password salah'
-        ], 401);
+            'token' => $token,
+            'token_type'   => 'Bearer',
+            'user' => $user,
+        ]);
     }
-
-    /** @var User $user */
-    $user = Auth::user();
-
-    $token = $user->createToken('react')->plainTextToken;
-
-    return response()->json([
-        'token' => $token,
-        'token_type'   => 'Bearer',
-        'user' => $user,
-    ]);
-}
 
     public function me(Request $request)
     {
         return $request->user();
     }
 
-    // public function logout(Request $request)
-    // {
-    //     $request->user()
-    //         ->currentAccessToken()
-    //         ->delete();
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken();
+        if ($token instanceof PersonalAccessToken) {
+            $token->delete();
+        }
 
-    //     return response()->json([
-    //         'message' => 'Logout berhasil'
-    //     ]);
-    // }
-
- 
+        return response()->json([
+            'message' => 'Logout success'
+        ]);
+    }
 }
